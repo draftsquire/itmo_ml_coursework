da7782ded8b837bff64d18fe8edd3ee10b2bd535
Ivan Kraev <kraev.ivan@cyberdrone.show>
2025-02-20 17:31:33 +0300

    adds coordinates array storing and a orgf reward function

--- gym_env_mecanum.py
+++ gym_env_mecanum.py
@@ -1,12 +1,13 @@
 from typing import Dict, Tuple, Union
 import os
 
-import mujoco
 import numpy as np
+import math
 
 from gymnasium import utils
 from gymnasium.envs.mujoco import MujocoEnv
 from gymnasium.spaces import Box
+from rtree.index import RT_MVRTree
 
 from scipy.spatial.transform import Rotation
 
@@ -18,23 +19,47 @@ DEFAULT_CAMERA_CONFIG = {
         "elevation": -50.0,
     }
 
-def calculate_phi_from_loc_x_axis(x_axis):
+def calculate_phi_from_loc_x_axis_deg(x_axis):
+    """Calculates phi angle (robot's rotation) from unit vector of robot's local X_axis
+
+    Args:
+        x_axis (numpy.ndarray or list): Unit vector from x-axis sensor, [x, y, z]
+
+    Returns:
+        float: Rotation between robot's X and global frame X in range [0, 360] degrees.
+    """
+    # Convert to NumPy array if not already
+    x_axis = np.array(x_axis, dtype=float)
+
+    # Use atan2 to calculate angle in radians
+    angle_rad = np.arctan2(x_axis[1], x_axis[0])
+
+    # Convert negative angles to positive by adding 360 degrees
+    angle_deg = np.degrees(angle_rad)
+    if angle_deg < 0:
+        angle_deg += 360
+
+    return angle_deg
+
+def calculate_phi_from_loc_x_axis_rad(x_axis):
     """Calculates phi angle (robot's rotation) from unit vector of robot's local X_axis
 
     Args:
         x_axis (numpy.ndarray):  unit vector from x-axis sensor, [x, y, z]
 
     Returns:
-        numpy.ndaray: counter-clockwise rotation between robot's X and global frame X [-pi; pi]
+        numpy.ndaray: counter-clockwise rotation between robot's X and global frame X [0; 2pi]
     """
     # Convert to NumPy array if not already
     
     np.array(x_axis, dtype=float)
     # Use atan2 to calculate angle directly
     angle_rad = np.arctan2(x_axis[1], x_axis[0])
-    
-    return angle_rad
+    # Convert negative angles to positive by adding 2π
+    if angle_rad < 0:
+        angle_rad += 2 * np.pi
 
+    return angle_rad
 
 class MecanumEnv(MujocoEnv, utils.EzPickle):
     """
@@ -54,30 +79,28 @@ class MecanumEnv(MujocoEnv, utils.EzPickle):
 
     def __init__(
         self,
+        coordinates, # Used for starting and target points
         mecanum_xml: str =  os.path.join(os.getcwd(),"mecanum.xml"),
         camera_config=DEFAULT_CAMERA_CONFIG,
-        init_pos=(0., 0.),
         reset_noise_scale=1.,
+        environment_boundary_radius=3.,
         **kwargs,
     ):
         utils.EzPickle.__init__(
             self,
             reset_noise_scale,
-            # forward_reward_weight,
-            # ctrl_cost_weight,
-            # healthy_reward,
-            # terminate_when_unhealthy,
-            # healthy_state_range,
-            # healthy_z_range,
-            # healthy_angle_range,
-            # reset_noise_scale,
-            # exclude_current_positions_from_observation,
+            coordinates,
+            reset_noise_scale,
+            environment_boundary_radius,
             **kwargs,
         )
 
         self._camera_config = camera_config
-        self._reset_noise_scale = reset_noise_scale
-
+        self._reset_noise_scale = reset_noise_scale # TODO: deprecated!
+        self._coordinates = coordinates
+        self._step_n = 0
+        self._episode_n = 0
+        self._environment_boundary_radius = environment_boundary_radius
         observation_space = Box(
                 low=-np.inf, high=np.inf, shape=(11,), dtype=np.float64
             )
@@ -89,67 +112,112 @@ class MecanumEnv(MujocoEnv, utils.EzPickle):
             default_camera_config=self._camera_config,
             **kwargs,
         )
+        # noise_low = -self._reset_noise_scale
+        # noise_high = self._reset_noise_scale
+        self._x_start = self._coordinates[0][0]
+        self._y_start = self._coordinates[0][1]
+        n = len(self._coordinates)
+        self._x_target = self._coordinates[n - 1][0]
+        self._y_target = self._coordinates[n - 1][1]
+        self._phi_target = 0.
 
-    @property
-    def terminated(self):
-        return False
+
+    # @property
+    # def terminated(self):
+    #     return False
 
     def _get_obs(self):
-        # without compensation it wont stabilize to 1,1,pi/2
         x, y = self.data.sensordata[0], self.data.sensordata[1]
+        x_t, y_t = self._x_target, self._y_target
         v_x, v_y = self.data.sensordata[3], self.data.sensordata[4]
         omega_z = self.data.sensordata[8]
         x_axis_data = np.array([self.data.sensordata[9], self.data.sensordata[10], self.data.sensordata[11]])
-        phi = calculate_phi_from_loc_x_axis(x_axis_data);
-        # e_x, e_y = 
-        # E_dist =
-        # e_phi = 
-        # position = self.data.qpos.flat.copy()
-        # velocity = np.clip(self.data.qvel.flat.copy(), -10, 10)
-        observation = np.array([x, y, v_x, v_y, omega_z, phi])
+        phi = calculate_phi_from_loc_x_axis_deg(x_axis_data)#from 0 to 360
+        e_x, e_y = x_t - x, y_t - y
+        E_dist = math.dist([x, y], [x_t, y_t])
+        e_phi = self._phi_target - phi
+
+        observation = np.array([x, y, v_x, v_y, omega_z, x_t, y_t, e_x, e_y, E_dist, e_phi])
         return observation
 
+    def ogrf(self, x_s, y_s, x_t, y_t, x, y, e_phi_norm, r, step, c_phi=0.4, max_steps=10000):
+        """
+        Orientational Gain Reward Function
+
+        :param x_s: Start point coordinate
+        :param y_s: Start point coordinate
+        :param x_t: Target point coordinate
+        :param y_t: Target point coordinate
+        :param x: Instantaneous robot's coordinate
+        :param y: Instantaneous robot's coordinate
+        :param e_phi_norm: Normalized orientation error
+        :param r: Environment boundary radius
+        :param step: Number of performed steps
+        :param c_phi: The orientation error constant
+        :param max_steps: Maximum number of steps
+
+        :return: tuple: (T, R_T) - A boolean indicating whether an episode is finished,
+         R_T - total reward obtained by the agent
+        """
+        T = False
+        R_T = 0
+        radius = x**2 + y**2
+        R1 = np.abs(x_s - x_t) - np.abs(x - x_t)
+        R2 = np.abs(y_s - y_t) - np.abs(y - y_t)
+        if (radius < r**2) and (e_phi_norm < c_phi):
+            R_T = (R1 + R2) * (1 - e_phi_norm)
+        else:
+            T = True
+            steps_left = max_steps - step
+            R_T = -steps_left
+
+        return T, R_T
+
+
     def step(self, action):
-        x_position_before = self.data.qpos[0]
         self.do_simulation(action, self.frame_skip)
-        x_position_after = self.data.qpos[0]
-        x_velocity = (x_position_after - x_position_before) / self.dt
-
-        # ctrl_cost = self.control_cost(action)
-
-        # healthy_reward = self.healthy_reward
-
-        rewards = 0 #+ healthy_reward
-        # costs = ctrl_cost
-
         observation = self._get_obs()
-        reward = rewards
-        terminated = self.terminated
-        info = {
-            "x_position": x_position_after,
-            "x_velocity": x_velocity,
-        }
+        # Unpack values into separate variables
+        x, y, v_x, v_y, omega_z, x_t, y_t, e_x, e_y, E_dist, e_phi_deg = observation
 
+        e_phi_norm = np.abs((e_phi_deg * (180 / np.pi)) / 180)
+        r = self._environment_boundary_radius # 2.0 by default
+
+        terminated, reward =  self.ogrf(self._x_start, self._y_start, x_t, y_t, x, y, e_phi_norm, r, self._step_n)
+        info = {
+            "x_position": 0,
+            "x_velocity": 0,
+        }
+        self._step_n += 1
         if self.render_mode == "human":
             self.render()
+
         return observation, reward, terminated, False, info
 
     def reset_model(self):
-        noise_low = -self._reset_noise_scale
-        noise_high = self._reset_noise_scale
-
+        """
+        Generates new initial and target coordinates by running a uniform distributed number generating function which will
+        add values within range of -1 to 1 to initial coordinates
+        :return: observations list
+        """
+        n = len(self._coordinates)
         qpos = self.init_qpos
-        qpos[0] = self.init_qpos[0] + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=1)#X-coordinate
-        qpos[1] = self.init_qpos[1] + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=1)#Y-coordinate
+        self._x_start = self._coordinates[self._episode_n][0]#X-coordinate
+        self._y_start = self._coordinates[self._episode_n][1]#Y-coordinate
+        qpos[0] = self._x_start
+        qpos[1] = self._y_start
+
         qpos[2] = self.init_qpos[2] # Z-coordinate remains zero (likely)
-        
+
+        self._x_target = self._coordinates[n - 1 - self._episode_n][0]
+        self._y_target = self._coordinates[n - 1 - self._episode_n][1]
+
         qvel = self.init_qvel
-
         self.set_state(qpos, qvel)
-
         observation = self._get_obs()
+        self._step_n = 0
+        # TODO: move to terminated function?
+        self._episode_n += 1
         return observation
 
     def viewer_setup(self):
