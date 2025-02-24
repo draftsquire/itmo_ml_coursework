9f47227a4c20031c0432ab155ff98f86bbe91112
drafty <kraev.i@niuitmo.ru>
2025-02-24 23:17:36 +0300

    implemented Quantile Regression DQN

---
+++ logger.py
@@ -0,0 +1,101 @@
+
+# MIT License
+
+# Copyright (c) 2018 Arsenii Ashukha
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import sys
+import random
+import numpy as np
+
+from collections import OrderedDict
+from tabulate import tabulate
+from pandas import DataFrame
+from time import gmtime, strftime
+
+
+class Logger:
+    def __init__(self, name='name', fmt=None):
+        self.handler = True
+        self.scalar_metrics = OrderedDict()
+        self.fmt = fmt if fmt else dict()
+
+        base = './logs'
+        if not os.path.exists(base): os.mkdir(base)
+
+        time = gmtime()
+        hash = ''.join([chr(random.randint(97, 122)) for _ in range(5)])
+        fname = '-'.join(sys.argv[0].split('/')[-3:])
+        self.path = '%s/%s-%s-%s-%s' % (base, fname, name, strftime('%m-%d-%H-%M', time), hash)
+
+        self.logs = self.path + '.csv'
+        self.output = self.path + '.out'
+        self.checkpoint = self.path + '.cpt'
+
+        def prin(*args):
+            str_to_write = ' '.join(map(str, args))
+            with open(self.output, 'a') as f:
+                f.write(str_to_write + '\n')
+                f.flush()
+
+            print(str_to_write)
+            sys.stdout.flush()
+
+        self.print = prin
+
+    def add_scalar(self, t, key, value):
+        if key not in self.scalar_metrics:
+            self.scalar_metrics[key] = []
+        self.scalar_metrics[key] += [(t, value)]
+
+    def add_dict(self, t, d):
+        for key, value in d.iteritems():
+            self.add_scalar(t, key, value)
+
+    def add(self, t, **args):
+        for key, value in args.items():
+            self.add_scalar(t, key, value)
+
+    def iter_info(self, order=None):
+        names = list(self.scalar_metrics.keys())
+        if order:
+            names = order
+        values = [self.scalar_metrics[name][-1][1] for name in names]
+        t = int(np.max([self.scalar_metrics[name][-1][0] for name in names]))
+        fmt = ['%s'] + [self.fmt[name] if name in self.fmt else '.1f' for name in names]
+
+        if self.handler:
+            self.handler = False
+            self.print(tabulate([[t] + values], ['step'] + names, floatfmt=fmt))
+        else:
+            self.print(tabulate([[t] + values], ['step'] + names, tablefmt='plain', floatfmt=fmt).split('\n')[1])
+
+    def save(self, silent=False):
+        result = None
+        for key in self.scalar_metrics.keys():
+            if result is None:
+                result = DataFrame(self.scalar_metrics[key], columns=['t', key]).set_index('t')
+            else:
+                df = DataFrame(self.scalar_metrics[key], columns=['t', key]).set_index('t')
+                result = result.join(df, how='outer')
+        result.to_csv(self.logs)
+        if not silent:
+            self.print('The log/output/model have been saved to: ' + self.path + ' + .csv/.out/.cpt')
---
+++ rl_utils.py
@@ -0,0 +1,52 @@
+
+# MIT License
+
+# Copyright (c) 2018 Arsenii Ashukha
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
+import torch 
+import random
+
+def huber(x, k=1.0):
+    return torch.where(x.abs() < k, 0.5 * x.pow(2), k * (x.abs() - 0.5 * k))
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, state, action, next_state, reward, done):
+        transition = torch.Tensor([state]), torch.tensor([action], dtype=torch.long), torch.Tensor([next_state]), torch.Tensor([reward]), torch.Tensor([done])
+        self.memory.append(transition)
+        if len(self.memory) > self.capacity: del self.memory[0]
+
+    def sample(self, batch_size):
+        sample = random.sample(self.memory, batch_size)
+        batch_state, batch_action, batch_next_state, batch_reward, batch_done = zip(*sample)
+        
+        batch_state = torch.cat(batch_state)
+        batch_action = torch.cat(batch_action)
+        batch_reward = torch.cat(batch_reward)
+        batch_done = torch.cat(batch_done)
+        batch_next_state = torch.cat(batch_next_state)
+        
+        return batch_state, batch_action, batch_reward.unsqueeze(1), batch_next_state, batch_done.unsqueeze(1)
+    
+    def __len__(self):
+        return len(self.memory)
